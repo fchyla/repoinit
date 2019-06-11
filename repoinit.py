8e9c7958dcdbd6ac6f02862f01b28ff634224c81
Filip Chyla <dende3@gmail.com>
2019-06-11 13:54:03 +0200

    added pygithub to interact with github

--- repoinit.py
+++ repoinit.py
@@ -1,1 +1,15 @@
 #!/usr/local/bin/python3
+
+
+import configparser
+from github import Github
+
+config = configparser.ConfigParser()
+config.read('repoinit.config')
+
+repo_key = config['GH']['K1'] #import gh key
+
+request = Github(repo_key)
+
+for repo in request.get_user().get_repos():
+    print(repo.name)
