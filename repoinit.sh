6d06330972f075ada6a4e9b83549282c97ba5c4a
Filip Chyla <dende3@gmail.com>
2019-06-11 13:02:27 +0200

    added create project dir and init

--- repoinit.sh
+++ repoinit.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
-PROJECTS_DIR="/Users/filipchyla/Documents/projects"
+PROJECTS_DIR="$HOME/projects"
 
 repoinit () {
 
+    if [[ $# -eq 0 ]] ; then
+        echo 'Missing project directory name, usage:'
+        echo 'repoinit <project_name>'
+        exit 0
+    fi
+
+    if [ ! -d "$PROJECTS_DIR" ]; then
+        echo "Created projects dir: $PROJECTS_DIR"
+        mkdir $PROJECTS_DIR
+    fi
+
     cd $PROJECTS_DIR
-    echo "hello"
-    echo $1
+
+
+    if [ -d "$1" ]; then
+        echo "$1 directory exists, select a different name"
+        exit 0
+    fi
+    mkdir -p $1
+    echo "Created $1"
+
+    cd $1
+    git init
+    git-crypt init
+    touch .gitignore .gitattributes
+    echo "# Syntax
+# secretfile filter=git-crypt diff=git-crypt
+# *.key filter=git-crypt diff=git-crypt" >> .gitattributes
 
 }
 
