9ab2c5f945f68cd85e019f6470b3d26efe7fb11a
Filip Chyla <dende3@gmail.com>
2020-09-25 15:28:23 +0200

    restructure and new features

--- repoinit.sh
+++ repoinit.sh
@@ -1,12 +1,36 @@
 #!/bin/bash
+
+add_pre_commit_check () {
+
+echo "repos:
+- repo: https://github.com/Yelp/detect-secrets.git
+  rev: v0.14.3
+  hooks:
+  - id: detect-secrets
+    args: ['--baseline', '.secrets.baseline']
+    exclude: .*/tests/.*" >> .pre-commit-config.yaml
+}
+
+add_gitignore() {
+    echo ".pre-commit-config.yaml" >> .gitignore
+}
+
+add_gitattributes() {
+
+    echo "# Syntax
+# secretfile filter=git-crypt diff=git-crypt
+# *.key filter=git-crypt diff=git-crypt" >> .gitattributes
+
+}
 repoinit () {
 
     git init
     git-crypt init
-    touch .gitignore .gitattributes
-    echo "# Syntax
-# secretfile filter=git-crypt diff=git-crypt
-# *.key filter=git-crypt diff=git-crypt" >> .gitattributes
+
+    add_pre_commit_check
+    add_gitignore
+    add_gitattributes
+
     git flow init -d
 }
 
